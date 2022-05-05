4302a811368668034014f86b06a41a352e768990
ElFerri <play3ferran@gmail.com>
2022-05-05 18:48:55 +0200

    Modificacions

---
+++ EntornosPruebaPatron/src/entornospruebapatron/EntornosPruebaPatron.java
@@ -0,0 +1,29 @@
+package entornospruebapatron;
+
+/**
+ *
+ * @author Ferran
+ */
+public class EntornosPruebaPatron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class Singleton {
+        private static Singleton instance = null;
+        private Singleton() {
+            //Este metodo existe solamente para evitar la instanciancion.
+        }
+        //Se utiliza el metodo getInstance para controlar la instanciancion.
+        //Solamente existira un objeto unico para la clase.
+        public static  Singleton getInstance(){
+            if (instance = null) {
+                instance = new
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
